ce1aec0250efd6ff8d0a73bda8d9b7f08cffb323
Владимир Солдатов <cbb110@rambler.ru>
2021-04-29 16:37:52 +0300

    First

--- main.py
+++ main.py
@@ -12,25 +12,24 @@ def read_xml(path, sheet, begin, end):
         a.drop(axis=1, columns='Value')
     else:
         a.columns = ['Name']
-    print(a.Name[500:600])
     for i in range(a.shape[0]):
         if type(a.Name[i]) == str:
             for y in range(10):
                 if ' ' + str(y) + ':' in a.Name[i]:
                     a.Name[i].replace(' ' + str(y) + ':', ' 0' + str(y) + ':')
-        if '0000' in a.Name[i]:
-            b = a.Name[i].split("  ")
-            if int(b[0]) == 10:
-                a.Name[i - 1] += b[2].strip()
-                a.Name[i] = pd.NA
-            elif int(b[0]) == 20:
-                a.Name[i - 2] += b[2]
-                a.Name[i] = pd.NA
-            else:
-                a.Name[i] = b[2]
+            if '0000' in a.Name[i]:
+                b = a.Name[i].split("  ")
+                if int(b[0]) == 10:
+                    a.Name[i - 1] += b[2].strip()
+                    a.Name[i] = pd.NA
+                elif int(b[0]) == 20:
+                    a.Name[i - 2] += b[2]
+                    a.Name[i] = pd.NA
+                else:
+                    a.Name[i] = b[2]
 
-        elif '[' in a.Name[i]:
-            a.Name[i] = datetime.strptime(a.Name[i].replace('[', '').replace(']', ''), '%d.%m.%Y %H:%M:%S:%f')
+            elif '[' in a.Name[i]:
+                a.Name[i] = datetime.strptime(a.Name[i].replace('[', '').replace(']', ''), '%d.%m.%Y %H:%M:%S:%f')
         else:
             a.Name[i] = pd.NA
     print('Первый этап закончен')
@@ -71,12 +70,14 @@ def read_xml(path, sheet, begin, end):
     print(end)
 
 
-begin_time = datetime.strptime('20.04.2021 10:31:30:000', '%d.%m.%Y %H:%M:%S:%f')
-end_time = datetime.strptime('21.04.2021 04:30:00:000', '%d.%m.%Y %H:%M:%S:%f')
-pathXls = 'C:\\Users\\СолдатовВВ\\Desktop\\Logs\\Отчеты\\Лист XLSX.xlsx'
-read_xml(pathXls, 'Спутник', begin_time, end_time)
-# read_xml(pathXls, 'GPRS', begin_time, end_time)
+begin_time = datetime.strptime('23.04.2021 09:03:16:000', '%d.%m.%Y %H:%M:%S:%f')
+end_time = datetime.strptime('24.04.2021 06:08:00:000', '%d.%m.%Y %H:%M:%S:%f')
+pathXls = 'C:\\Users\\СолдатовВВ\\Desktop\\Logs\\Отчеты\\Лист XLSX 22-24.04.2021.xlsx'
+#read_xml(pathXls, 'Спутник', begin_time, end_time)
+read_xml(pathXls, 'GPRS', begin_time, end_time)
 # read_xml(pathXls, 'Лист2')
 
 # begin_time = datetime.strptime('19.04.2021 10:31:30:000', '%d.%m.%Y %H:%M:%S:%f')
 # end_time = datetime.strptime('20.04.2021 09:35:16:000', '%d.%m.%Y %H:%M:%S:%f')
+# begin_time = datetime.strptime('20.04.2021 10:31:30:000', '%d.%m.%Y %H:%M:%S:%f')
+# end_time = datetime.strptime('21.04.2021 04:30:00:000', '%d.%m.%Y %H:%M:%S:%f')
